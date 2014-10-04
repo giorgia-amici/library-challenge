43d07753329da68f6283015edf05fce08fcab419
Giorgia <giorgia1london@gmail.com>
2014-10-04 12:26:41 +0100

    collection.rb

---
+++ collection.rb
@@ -0,0 +1,41 @@
+class Library
+	#attr_accessor :library_collection
+
+	def initialize
+		@library = []
+	end
+
+
+
+#call the following method first then add the objects to the list
+	def get_collection
+		@library
+	end
+
+
+
+
+
+end
+
+
+#count of how many books are in the library's collection
+
+#first I need to store my collection in a list? or an hash?
+
+
+#attempt with list
+
+
+
+
+
+
+
+=begin
+when I have tried to push my object into my array, it did not allow me and I had to do
+	collection.library_collection << @sherlock
+	but before I had already do 
+	collection.library_collection = []
+	
+=end
