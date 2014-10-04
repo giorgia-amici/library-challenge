b132b8a808347fdcef7e6367011153f3cb90001d
Giorgia <giorgia1london@gmail.com>
2014-10-05 00:07:49 +0100

    book.rb

--- book.rb
+++ book.rb
@@ -1,16 +1,6 @@
 class Book
-	attr_accessor :title, :author, :isbn, :max_loan
-
-
-
-
-=begin
-loan length = does it make sense to put it in the attr_accessor? Because imagine that
-someone wants an extension, you can still change it
-=end
-
-
-
+	attr_accessor :title, :author, :isbn, :max_loan, :available, :is_broken
 
 
 end
+
