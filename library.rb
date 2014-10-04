e246d8229c045e638631c2c076508f9b9cf8fc85
Giorgia <giorgia1london@gmail.com>
2014-10-05 00:04:53 +0100

    library.rb

--- library.rb
+++ library.rb
@@ -12,7 +12,27 @@ class Library
 		@inventory.size
 	end		
 
+	def add_book_inventory(book)
+		@inventory << book
+		book.available = true
+	end
 
+	def available_books
+		@available_books = []
+		@inventory.each do |book| 
+			@available_books << book if book.available == true
+		end
+		@available_books
+		"We have #{@available_books.size} available books" 
+	end
+
+	def mantainance_needed
+		@mantainance_needed_items = []
+		@inventory.each do |book|
+			@mantainance_needed_items << book if book.is_broken == true
+		end
+		@mantainance_needed_items
+	end
 
 end
 
