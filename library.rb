f63a1aba8e60d0b7552ff3e7747411dbe22a5a91
Giorgia <giorgia1london@gmail.com>
2014-10-04 14:25:57 +0100

    library.rb

---
+++ library.rb
@@ -0,0 +1,35 @@
+class Library
+	attr_accessor :inventory, :location
+
+	def initialize
+		@inventory = []
+	end
+
+
+#call the following method first then add the objects to the list and along with
+#size() can count the items in the library
+
+
+
+
+
+	def numer_of_books
+		@inventory.size
+	end		
+
+
+
+
+
+end
+
+
+#count of how many books are in the library's collection
+
+#first I need to store my collection in a list? or an hash?
+#attempt with list(done)
+#to do: attemp using an hash that will have 3 symbols :title, :author and :isbn
+
+
+
+
