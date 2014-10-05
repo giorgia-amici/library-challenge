c1ba8cba0e546a6af8592ce960d2ab4ef9009a30
Giorgia <giorgia1london@gmail.com>
2014-10-05 18:56:27 +0100

    member.rb

--- member.rb
+++ member.rb
@@ -6,21 +6,6 @@ class Member
 		@surname = surname
 		@loan_history = []
 	end
-
-	def borrowing_books(book)
-		@start_loan_date = Date.today
-		@end_loan_date = @start_loan_date + 30
-		@loan_history << @surname + " " + @name + " / " + "Item ISBN: " + book.isbn + " / " + "End of loan date : " + @end_loan_date.to_s
-		book.available = false
-		book.borrower = @surname + " " + @name
-
-	end
-
-	def start_loan_date
-		@start_loan_date
-	end
-
-
 end
 
 
